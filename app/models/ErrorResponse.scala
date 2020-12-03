9086ce65584d63b90e94dc9ba5c33c9d724d1fe0
DevAndyroo <andy@andyroo.info>
2020-12-03 15:25:17 +0000

    Bring failure responses inline with samples of schema

--- app/models/ErrorResponse.scala
+++ app/models/ErrorResponse.scala
@@ -29,10 +29,10 @@ object ErrorResponse {
 object FailureMessage {
   implicit val reads: OFormat[FailureMessage] = Json.format[FailureMessage]
 
-  val InvalidJson = FailureMessage("InvalidJson", "The JSON provided was not valid.")
-  val ServerError = FailureMessage("ServerError", "An Internal Server Error occured.")
-  val ServiceUnavailable = FailureMessage("ServiceUnavailable", "Service is not available.")
-  val Unauthorized = FailureMessage("Unauthorized","Request Unauthorized.")
-  val MissingBearerToken = FailureMessage("MissingBearerToken","Bearer token is missing.")
-  val MissingBody = FailureMessage("MissingBody","There was no body provided.")
+  val InvalidJson: FailureMessage = FailureMessage("INVALID_PAYLOAD", "Submission has not passed validation. Invalid payload.")
+  val ServerError: FailureMessage = FailureMessage("SERVER_ERROR", "IF is currently experiencing problems that require live service intervention.")
+  val ServiceUnavailable: FailureMessage = FailureMessage("SERVICE_UNAVAILABLE", "Dependent systems are currently not responding.")
+  val Unauthorized: FailureMessage = FailureMessage("UNAUTHORIZED", "Request Unauthorized.")
+  val MissingBearerToken: FailureMessage = FailureMessage("MISSING_BEARER_TOKEN", "Bearer token is missing.")
+  val MissingBody: FailureMessage = FailureMessage("MISSING_BODY", "There was no body provided.")
 }
